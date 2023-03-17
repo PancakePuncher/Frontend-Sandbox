4cca402050616022f8ff0cbdf8ca8dd7b652b8b3
PancakePuncher <PancakePuncher@gmail.com>
2023-03-17 18:25:07 +0000

    Adjusted Request to have own sessions

--- fastapi/routes/item/item.py
+++ fastapi/routes/item/item.py
@@ -1,6 +1,12 @@
 import strawberry
 import random
-from database.utility.db_util_init import db_connection, Items, all_item_ids
+from database.utility.db_util_init import (
+    master_db,
+    Items,
+    Questions,
+    all_item_ids,
+    all_question_ids,
+)
 from strawberry.asgi import GraphQL
 
 
@@ -14,19 +20,24 @@ class Item:
 
 @strawberry.type
 class Question:
-    question: str = None
+    questionId: int = None
+    questionText: str = None
+    questionOff: int = None
+    questionT: int = None
+    questionF: int = None
 
 
 @strawberry.type
 class Query:
     @strawberry.field
     async def randItem(self) -> Item:
-        if len(all_item_ids) > 0:
+
+        if len(all_item_ids) > 1:
             all_item_ids.pop(0)
             random_item = random.choice(all_item_ids)
         else:
             random_item = 0
-        async with db_connection:
+        async with master_db.connection():
             item = await Items.select().where(Items.pk_item_id == random_item).get()
         return Item(
             itemId=item.pk_item_id,
@@ -38,7 +49,24 @@ class Query:
     @strawberry.field
     async def randQuestion(self) -> Question:
 
-        return Question(question="I am a random question.")
+        if len(all_question_ids) > 1:
+            all_question_ids.pop(0)
+            random_question = random.choice(all_question_ids)
+        else:
+            random_question = 0
+        async with master_db.connection():
+            question = (
+                await Questions.select()
+                .where(Questions.pk_question_id == random_question)
+                .get()
+            )
+        return Question(
+            questionId=question.pk_question_id,
+            questionText=question.question_text_str,
+            questionOff=question.question_offered_int,
+            questionT=question.question_truthy_int,
+            questionF=question.question_falsy_int,
+        )
 
 
 item_schema = GraphQL(strawberry.Schema(query=Query))
